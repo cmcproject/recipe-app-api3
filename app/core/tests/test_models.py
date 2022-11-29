8c574cb46b4b4a66611e0dbc78fa234b63022278
marius.crisan <marius.crisan.tech@gmail.com>
2022-11-29 09:13:27 +0200

    Handle error

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,12 @@
 """
 Tests for models
 """
+from decimal import Decimal
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from core import models
 
 
 class ModelTests(TestCase):
@@ -29,9 +33,9 @@ class ModelTests(TestCase):
             ['test4@example.COM', 'test4@example.com'],
         ]
 
-        for email, expacted in sample_emails:
+        for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
-            self.assertEqual(user.email, expacted)
+            self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raoses_error(self):
         """Test that creating a user without an email raises a ValueError"""
@@ -47,3 +51,19 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful"""
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass123',
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+            price= Decimal('5.50'),
+            description='Sample recipe description',
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
